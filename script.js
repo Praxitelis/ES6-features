39fef99c8462aed330d114b98d5f4f41960149b7
Praxitelis <praxitelis.nikolaou@gmail.com>
2018-08-25 22:45:56 +0300

    Destructuring

--- script.js
+++ script.js
@@ -301,3 +301,35 @@ var friends = ['Bob', 'Jane', 'Mark'];
 
 new Person('Mike').myFriends6(friends);
 */
+
+
+//////////////////////////////////////////////////////////////////////
+//// DESTRUCTURING
+
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+//var age = john[1];
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRretirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRretirement(1990);
+console.log(age2);
+console.log(retirement);
