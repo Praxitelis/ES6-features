843722c83ae8fe046a526e7a99ee51aa8fa7d5b7
Praxitelis <praxitelis.nikolaou@gmail.com>
2018-08-25 22:18:27 +0300

    Arrow Functions Lexical This Keyword

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ console.log(`${firstName} `.repeat(5)); // repeats with a space
 
 ///////////////////////////////////////////////
 //// ARROW FUNCTIONS 1
-
+/*
 const years = [1990, 1986, 1982, 1937];
 
 // ES5
@@ -180,3 +180,124 @@ ages6 = years.map((el, index) => {
 
 });
 console.log(ages6);
+
+*/
+
+
+//////////////////////////////////////////////////////
+/////// ARROW FUNCTIONS: Lexical 'this' Keyword
+
+// Arrow functions dont have their own 'this' but they use the this keyword of the function
+// they are written in.
+
+// ES5
+
+/*
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+
+}
+box5.clickMe();
+
+// to avoid this situation we can use this work around
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+
+}
+box5.clickMe();
+
+*/
+
+// ES6
+// by using the arrow function we have access to the this keyword, because the arrow function
+// shares the this keyword with its surrounding code
+var box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+
+}
+// box6.clickMe();
+
+
+// turning the clickMe into an arrow function will throw undefined because now this arrow
+// function will share the this keyword with its surroundings, which in this case it is the 
+// global object window.
+/*
+
+var box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+
+}
+box66.clickMe();
+*/
+
+// The following wouldnt work without the bind because the this keyword points 
+//to the global object, and not to the Person. With bind we manually bind this keyword
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+/*
+
+Person.prototype.myFriends5 = function(friends) {
+
+    var arr = friends.map(function(el){
+
+        return this.name + ' is friends with ' + el;
+
+    }.bind(this));
+    console.log(arr);
+}
+
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('John').myFriends5(friends);
+*/
+
+
+// ES6
+/*
+Person.prototype.myFriends6 = function(friends) {
+
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+      
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('Mike').myFriends6(friends);
+*/
